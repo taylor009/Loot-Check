480f89cd40781fa5f3cdeed411198a2792ee0fbc
Taylor Gagne <taylor@baremetalcloud.com>
2018-05-05 17:04:54 -0400

    Created first redux action for balance along with a test case for the redux action.

---
+++ src/actions/balance.test.js
@@ -0,0 +1,11 @@
+import * as constants from './constants';
+import * as actions from './balance';
+
+
+it('creates and actions to set the balance', () => {
+  const balance = 0;
+
+  const expectedAction = {type: constants.SET_BALANCE, balance};
+
+  expect(actions.setBalance(balance)).toEqual(expectedAction);
+});
