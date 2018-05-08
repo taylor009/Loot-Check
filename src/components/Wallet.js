a2ddcf15afdd4c6334a5b72d6bd7474fd08e5923
Taylor Gagne <taylor@baremetalcloud.com>
2018-05-08 14:01:26 -0400

    Added local wallet changes to the user interface

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -3,10 +3,20 @@ import {connect} from 'react-redux';
 
 
 export class Wallet extends React.Component{
+  constructor(){
+    super();
+
+    this.state = {balance: undefined};
+  }
+
+  updateBalance = event => this.setState({balance: parseInt(event.target.value, 10)})
+
   render(){
     return(
       <div>
         <h3 className='balance'>Wallet balance: {this.props.balance}</h3>
+        <br/>
+        <input onChange={this.updateBalance} className='input-wallet'/>
       </div>
     )
   }
